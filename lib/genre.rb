46895d28008e7acca8e4d073b0288a8517470466
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-27 05:18:42 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,7 +1,13 @@
 class Genre
   attr_accessor :name, :songs
+
   def initialize(name)
     @name = name
     @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.genre = self
   end
 end
